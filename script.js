c28aa5725327af09004c27c445294a51a4ca1f69
sacaronte111110-dotcom <sacaronte111110@gmail.com>
2026-01-18 02:50:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Hoops5 funcionando correctamente");
+const boton = document.querySelector("button");
+const mensaje = document.getElementById("mensaje");
+
+boton.addEventListener("click", () => {
+  mensaje.textContent = "Intento registrado 🏀";
+});
